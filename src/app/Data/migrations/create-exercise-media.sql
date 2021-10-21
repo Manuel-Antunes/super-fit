879cad6408fa6269e7f163b4acd0b1dd4695c1f5
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-10-21 02:32:29 -0300

    fix: update migrations

--- src/app/Data/migrations/create-exercise-media.sql
+++ src/app/Data/migrations/create-exercise-media.sql
@@ -1,7 +1,7 @@
-CREATE TABLE exercises_media(
+CREATE TABLE exercise_media(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     media_id INTEGER NOT NULL UNIQUE,
-    exercises_id INTEGER NOT NULL UNIQUE,
+    exercise_id INTEGER NOT NULL UNIQUE,
     FOREIGN KEY (media_id) REFERENCES media(id),
-    FOREIGN KEY (exercises_id) REFERENCES exercises(id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
 );
